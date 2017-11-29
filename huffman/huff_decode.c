7a053d1b80108d28b27d0b20a985b112c7509b82
e-mez <emez.uwalaka@gmail.com>
2017-11-29 14:44:18 +0100

    Decoder() works

--- huffman/huff_decode.c
+++ huffman/huff_decode.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
-
 #include "arbrebin.h"
 #include "bfile.h"
 #include "assert.h"
 
 FILE * fichier_encode;
+FILE * fichier_decode;
 
 Arbre ArbreHuffman;
 
 void Decoder() {
-  AfficherArbre(ArbreHuffman);
-  /* A COMPLETER */
-  printf("Programme non realise (Decoder)\n");  
+	BFILE *bitfile;
+	int bit;
+	Arbre parcours;
+	
+	ArbreHuffman = LireArbre(fichier_encode);
+	AfficherArbre(ArbreHuffman);
+	bitfile = bstart(fichier_encode,"r");
+	parcours = ArbreHuffman;
+	
+	while(!feof(fichier_encode)){
+		bit = bitread(bitfile);
+
+		if(bit == 0)
+			parcours = FilsGauche(parcours);
+		else
+			parcours = FilsDroit(parcours);
+
+		if(EstVide(FilsGauche(parcours)) && EstVide(FilsDroit(parcours))){
+			fputc((int)Etiq(parcours),fichier_decode);
+			parcours = ArbreHuffman;
+		}
+	}
 }
 
 int main(int argc, char * argv[]) {
-  fichier_encode = fopen(argv[1], "r");
+	fichier_encode = fopen(argv[1], "r");
+	fichier_decode = fopen(argv[2], "w");
 
-  ArbreHuffman = LireArbre(fichier_encode);
+	Decoder();
 
-  Decoder();
-
-  fclose(fichier_encode);
+	fclose(fichier_encode);
+	fclose(fichier_decode);
   
-  return 0;
+	return 0;
 }
